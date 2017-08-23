972c53cedf187d501c3bb0b208cdf815c060f405
drazen41 <drazen.svec@gmail.com>
2017-08-23 17:04:49 +0200

    Pushit

---
+++ MVC-Test/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        People[] people = new People[]
+        {
+             new People() { Name = "Test", Location = "Test"},
+             new People() {Name = "Test 1", Location = "Test1"}, 
+             new People () { Name = "Test 2", Location = "Test 2"}
+        };
+        // GET: api/People
+        
+
+        // GET: api/People/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+        public IEnumerable<People>  GetAllPeople()
+        {
+            return people;
+        }
+        
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/People/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/People/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MVC-Test/Models/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Test.Models
+{
+    public class People
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
